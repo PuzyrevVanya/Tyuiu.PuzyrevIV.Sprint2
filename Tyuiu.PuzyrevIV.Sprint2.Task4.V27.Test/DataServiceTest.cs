7af9223fbea296725d8b0126773f9e43f7b27d28
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-10-16 22:47:03 +0500

    Task4

---
+++ Tyuiu.PuzyrevIV.Sprint2.Task4.V27.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PuzyrevIV.Sprint2.Task4.V27.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint2.Task4.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            double res = ds.Calculate(x, y);
+            double wait = 24.96;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 20;
+            double res = ds.Calculate(x, y);
+            double wait = 399.96;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PuzyrevIV.Sprint2.Task4.V27/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PuzyrevIV.Sprint2.Task4.V27.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint2.Task4.V27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                        *");
+            Console.WriteLine("*******************************************");
+
+            Console.WriteLine("Введите значения переменой X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значения переменой Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                              *");
+            Console.WriteLine("*******************************************");
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
